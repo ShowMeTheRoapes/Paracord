a26d623d0a4cb38d1d63a5ce69ba9f60810a0ed6
Dustin <dustin.a.roan@gmail.com>
2019-07-02 22:56:45 -0500

    Added java unit test and formatter stuff to the pom (#4)
    * Added formatters and tests to server project
    
    * Adding
    'yarn' to the precommit and prepush commands
    
    * Fixing some package.json issues

--- server/src/main/java/com/showmetheroapes/example/data/UserController.java
+++ server/src/main/java/com/showmetheroapes/example/data/UserController.java
@@ -6,12 +6,11 @@ import com.showmetheroapes.example.domain.User;
  * Controller used to access data from the Github API.
  *
  * @author Dustin Roan (dustin.a.roan@gmail.com)
- *
  */
 public class UserController {
 
-	public User getAUser(final String userId) {
-		// Do something here
-		return new User.Builder().user(userId).build();
-	}
+  public User getAUser(final String userId) {
+    // Do something here
+    return new User.Builder().user(userId).build();
+  }
 }
--- server/src/main/java/com/showmetheroapes/example/resource/UserResource.java
+++ server/src/main/java/com/showmetheroapes/example/resource/UserResource.java
@@ -1,15 +1,14 @@
 package com.showmetheroapes.example.resource;
 
+import com.google.gson.Gson;
+import com.showmetheroapes.example.data.UserController;
+import com.showmetheroapes.example.domain.User;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-import com.google.gson.Gson;
-import com.showmetheroapes.example.data.UserController;
-import com.showmetheroapes.example.domain.User;
 
 /**
  * Resource class for retrieving information.
@@ -19,12 +18,12 @@ import com.showmetheroapes.example.domain.User;
 @Path("/example")
 @Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
-	private final UserController controller = new UserController();
+  private final UserController controller = new UserController();
 
-	@GET
-	@Path("/users/{userId}")
-	public Response getUserFollowersAndSubfollowers(@PathParam("userId") final String userId) {
-		final User user = controller.getAUser(userId);
-		return Response.ok(new Gson().toJson(user)).build();
-	}
+  @GET
+  @Path("/users/{userId}")
+  public Response getUserFollowersAndSubfollowers(@PathParam("userId") final String userId) {
+    final User user = controller.getAUser(userId);
+    return Response.ok(new Gson().toJson(user)).build();
+  }
 }
