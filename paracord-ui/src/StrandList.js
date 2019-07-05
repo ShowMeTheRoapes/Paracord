dda883c693e3773f0796c309916984b6ef9384f2
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2019-07-04 23:54:00 -0700

    Renders a StrandList component inside of the App

---
+++ paracord-ui/src/StrandList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function StrandList() {
+   return (
+      <div id='strand-list'className='bg-gray-600'>
+         <p>I will be the strand list one day!</p>
+      </div>
+   )
+}
+
+export default StrandList
--- paracord-ui/src/__tests__/App.test.js
+++ paracord-ui/src/__tests__/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
 import App from '../App'
+import StrandList from '../StrandList'
 
 it('renders without crashing', () => {
    const div = document.createElement('div')
    ReactDOM.render(<App />, div)
    ReactDOM.unmountComponentAtNode(div)
+})
+
+it('contains a StrandList', () => {
+   const wrapper = shallow(<App />)
+   expect(wrapper.find(StrandList).length).toBe(1)
 })
