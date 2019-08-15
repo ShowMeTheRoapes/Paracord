369fc80c271ca3a87727942ae5b978a27a26c2a0
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2019-08-14 22:19:48 -0700

    Replace Jersey with spring-boot for rest service

---
+++ server/src/main/java/com/showmetheroapes/paracord/ParacordApplication.java
@@ -0,0 +1,12 @@
+package com.showmetheroapes.paracord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParacordApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ParacordApplication.class, args);
+  }
+}
---
+++ server/src/test/java/com/showmetheroapes/paracord/ParacordApplicationTests.java
@@ -0,0 +1,14 @@
+package com.showmetheroapes.paracord;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ParacordApplicationTests {
+
+  @Test
+  public void contextLoads() {}
+}
