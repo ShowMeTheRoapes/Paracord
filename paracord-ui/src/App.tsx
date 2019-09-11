5e241844d44362f9fc7cd995c02463c4845e6968
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2019-09-10 22:01:20 -0700

    Linting

--- paracord-ui/src/App.tsx
+++ paracord-ui/src/App.tsx
@@ -1,16 +1,17 @@
-
 import React from 'react'
 import './generated/tailwind.css'
 import StrandList from './Components/StrandList'
 
 const App: React.FunctionComponent<{}> = () => {
-   return (
-      <div className='flex'>
-         <div className='w-1/6 bg-gray-500' />
-         <div className='w-4/6 bg-gray-500'><StrandList /></div>
-         <div className='w-1/6 bg-gray-500' />
+  return (
+    <div className="flex">
+      <div className="w-1/6 bg-gray-500" />
+      <div className="w-4/6 bg-gray-500">
+        <StrandList />
       </div>
-   )
+      <div className="w-1/6 bg-gray-500" />
+    </div>
+  )
 }
 
 export default App
