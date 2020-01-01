49268301d59a10dda784bd10e99bc18204588551
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-01-01 16:56:58 -0600

    Corrects the name of a Strand test

--- paracord-ui/src/__tests__/Strand.test.tsx
+++ paracord-ui/src/__tests__/Strand.test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import Strand from '../Components/Strand'
 
-it('contains a the passed in name', () => {
+it('contains the passed in name', () => {
    const wrapper = shallow(<Strand name='Test' />)
    expect(wrapper.contains('Test')).toBe(true)
 })
